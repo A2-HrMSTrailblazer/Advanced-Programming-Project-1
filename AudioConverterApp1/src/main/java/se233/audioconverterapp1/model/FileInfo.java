f813327bb9fe995432d04f03ba15a5f592034e52
HrMSTrailblazer <aungkhaingphyo_1@cmu.ac.th>
2025-09-27 16:49:32 +0700

    update: FileInfo updated

--- AudioConverterApp1/src/main/java/se233/audioconverterapp1/model/FileInfo.java
+++ AudioConverterApp1/src/main/java/se233/audioconverterapp1/model/FileInfo.java
@@ -1,5 +1,27 @@
 package se233.audioconverterapp1.model;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class FileInfo {
-    
+    private final StringProperty fileName;
+    private final StringProperty format;
+    private final StringProperty size;
+    private final DoubleProperty progress;
+
+    public FileInfo(String fileName, String format, String size) {
+        this.fileName = new SimpleStringProperty(fileName);
+        this.format = new SimpleStringProperty(format);
+        this.size = new SimpleStringProperty(size);
+        this.progress = new SimpleDoubleProperty(0.0);
+    }
+
+    public StringProperty fileNameProperty() { return fileName; }
+    public StringProperty formatProperty() { return format; }
+    public StringProperty sizeProperty() { return size; }
+    public DoubleProperty progressProperty() { return progress; }
+
+    public void setProgress(double value) { this.progress.set(value); }
 }
