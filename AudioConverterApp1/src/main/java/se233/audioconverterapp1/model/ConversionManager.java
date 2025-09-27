75530a7699994b9745050e0f2300530361a99916
HrMSTrailblazer <aungkhaingphyo_1@cmu.ac.th>
2025-09-27 16:51:53 +0700

    update: ConversionManager file updated

--- AudioConverterApp1/src/main/java/se233/audioconverterapp1/model/ConversionManager.java
+++ AudioConverterApp1/src/main/java/se233/audioconverterapp1/model/ConversionManager.java
@@ -1,5 +1,22 @@
 package se233.audioconverterapp1.model;
 
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+
 public class ConversionManager {
-    
+
+    public void startConversions(ObservableList<FileInfo> files, String outputFormat, Runnable onProgressUpdate) {
+        for (FileInfo info : files) {
+            ConversionTask task = new ConversionTask(info, outputFormat);
+
+            // Update global progress after each progress update
+            task.progressProperty().addListener((obs, oldVal, newVal) ->
+                    Platform.runLater(onProgressUpdate)
+            );
+
+            Thread t = new Thread(task);
+            t.setDaemon(true);
+            t.start();
+        }
+    }
 }
