e5aa9ade0576f1d3ebc7bc2eaa9b99beca38f2b7
HrMSTrailblazer <aungkhaingphyo_1@cmu.ac.th>
2025-09-27 22:54:31 +0700

    update: status

--- AudioConverterApp1/src/main/java/se233/audioconverterapp1/model/FileInfo.java
+++ AudioConverterApp1/src/main/java/se233/audioconverterapp1/model/FileInfo.java
@@ -10,23 +10,22 @@ public class FileInfo {
     private final StringProperty format;
     private final StringProperty size;
     private final DoubleProperty progress;
+    private final StringProperty status;
 
     public FileInfo(String fileName, String format, String size) {
         this.fileName = new SimpleStringProperty(fileName);
         this.format = new SimpleStringProperty(format);
         this.size = new SimpleStringProperty(size);
         this.progress = new SimpleDoubleProperty(0.0);
+        this.status = new SimpleStringProperty("Pending");
     }
 
     public StringProperty fileNameProperty() { return fileName; }
     public StringProperty formatProperty() { return format; }
     public StringProperty sizeProperty() { return size; }
     public DoubleProperty progressProperty() { return progress; }
+    public StringProperty statusProperty() { return status; }
 
     public void setProgress(double value) { this.progress.set(value); }
-
-    public void setStatus(String string) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setStatus'");
-    }
+    public void setStatus(String value) { this.status.set(value); }
 }
